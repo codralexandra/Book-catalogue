a44ab7f255e92a91371306e71e5907a511be708a
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2025-01-06 19:25:51 +0200

    Fixed search + result display

--- book-catalogue/app.js
+++ book-catalogue/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     var item, title, author, publisher, bookLink, bookImg;
     var outputList = document.getElementById('list-output');
-    var bookUrl = "https://openlibrary.org/search.json?q=";
-    var placeHldr = '<img src="./assets/placeholder.jpg" alt="Book Cover">';
+    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    var placeHldr = './assets/images/placeholder.jpeg';
     var searchData;
 
     $("#search").click(function(){
@@ -14,11 +14,11 @@ $(document).ready(function(){
         }
         else{
             $.ajax({
-                url: bookUrl + searchData + "&limit=10",
+                url: bookUrl + encodeURIComponent(searchData) + "&maxResults=6",
                 dataType: "json",
                 success: function(response){
                     console.log(response);
-                    if(response.num_found === 0){
+                    if(response.totalItems === 0){
                         alert("No results! Try again.");
                     }
                     else{
@@ -36,60 +36,47 @@ $(document).ready(function(){
     });
 
     function displayResults(response) {
-        const docs = response.docs;
-        for (let i = 0; i < docs.length; i += 2) {
-            let item1 = docs[i];
-            let title1 = item1.title || "No Title Available";
-            let author1 = item1.author_name ? item1.author_name.join(", ") : "Unknown Author";
-            let publisher1 = item1.publisher ? item1.publisher.join(", ") : "Unknown Publisher";
-            let bookLink1 = "https://openlibrary.org" + item1.key || "#";
-            let bookImg1 = item1.cover_i 
-                            ? `https://covers.openlibrary.org/b/id/${item1.cover_i}-M.jpg` 
-                            : placeHldr;
-
-            let title2, author2, publisher2, bookLink2, bookImg2;
-            if (i + 1 < docs.length) {
-                let item2 = docs[i + 1];
-                title2 = item2.title || "No Title Available";
-                author2 = item2.author_name ? item2.author_name.join(", ") : "Unknown Author";
-                publisher2 = item2.publisher ? item2.publisher.join(", ") : "Unknown Publisher";
-                bookLink2 = "https://openlibrary.org" + item2.key || "#";
-                bookImg2 = item2.cover_i 
-                            ? `https://covers.openlibrary.org/b/id/${item2.cover_i}-M.jpg` 
-                            : placeHldr;
-            } else {
-                title2 = author2 = publisher2 = bookLink2 = "N/A";
-                bookImg2 = placeHldr;
+        const books = response.items;
+        let rowHTML = '';
+        for (let i = 0; i < books.length; i++) {
+            let book = books[i].volumeInfo;
+            let title = book.title || "No Title Available";
+            let author = book.authors ? book.authors.join(", ") : "Unknown Author";
+            let publisher = book.publisher || "Unknown Publisher";
+            let bookLink = book.infoLink || "#";
+            let bookImg = book.imageLinks && book.imageLinks.thumbnail
+                          ? book.imageLinks.thumbnail
+                          : placeHldr;
+    
+            // Append the card to the rowHTML
+            rowHTML += formatOutput(bookImg, title, author, publisher, bookLink);
+    
+            // Every 3 books or at the end of the list, close the row and append to outputList
+            if ((i + 1) % 3 === 0 || i === books.length - 1) {
+                outputList.innerHTML += `<div class="row mt-4">${rowHTML}</div>`;
+                rowHTML = ''; // Reset for the next row
             }
-
-            outputList.innerHTML += '<div class="row mt-4">' +
-                                    formatOutput(bookImg1, title1, author1, publisher1, bookLink1) +
-                                    formatOutput(bookImg2, title2, author2, publisher2, bookLink2) +
-                                    '</div>';
         }
     }
 
     function formatOutput(bookImg, title, author, publisher, bookLink) {
         var placeholder = './assets/images/placeholder.jpeg';
         var imageSrc = bookImg || placeholder;
-    
-        var htmlCard = `<div class="col-lg-6">
-              <div class="card">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img src="${imageSrc}" class="card-img" alt="Book Cover">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${title}</h5>
-                      <p class="card-text">Author: ${author}</p>
-                      <p class="card-text">Publisher: ${publisher}</p>
-                      <a target="_blank" href="${bookLink}" class="btn btn-secondary">View Book</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>`;
+
+        var htmlCard = `<div class="col-lg-4 col-md-6 mb-4"> <!-- col-lg-4 for 3 items per row -->
+          <div class="card">
+            <div class="card-body d-flex flex-column justify-content-between"> <!-- Use flexbox for consistent layout -->
+              <!-- Book Image -->
+              <img src="${imageSrc}" class="card-img-top mx-auto" alt="Book Cover" style="max-width: 200px; margin-bottom: 20px;">
+              <!-- Title -->
+              <h5 class="card-title">${title}</h5>
+              <!-- Author -->
+              <p class="card-text">Author: ${author}</p>
+              <!-- Book Link Button -->
+              <a target="_blank" href="${bookLink}" class="btn btn-secondary mt-3">View Book</a>
+            </div>
+          </div>
+        </div>`;
         return htmlCard;
     }
 
