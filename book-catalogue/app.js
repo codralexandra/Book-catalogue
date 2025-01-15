8778b556a2d27c006bcff0998a7b508551b3dbcf
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2025-01-15 09:09:39 +0200

    changed api (again)

--- book-catalogue/app.js
+++ book-catalogue/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    var item, title, author, publisher, bookLink, bookImg;
     var outputList = document.getElementById('list-output');
-    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    var bookUrl = "https://openlibrary.org/search.json?q=";
     var placeHldr = './assets/images/placeholder.jpeg';
     var searchData;
 
@@ -11,17 +10,14 @@ $(document).ready(function(){
 
         if(searchData === "" || searchData === null){
             displayError();
-        }
-        else{
+        } else {
             $.ajax({
-                url: bookUrl + "intitle:" + '"' + encodeURIComponent(searchData) + '"' + "&printType=books&maxResults=6&langRestrict=en",
+                url: bookUrl + encodeURIComponent(searchData) + "&limit=6",
                 dataType: "json",
                 success: function(response){
-                    console.log(response);
-                    if(response.totalItems === 0){
+                    if(response.numFound === 0){
                         alert("No results! Try again.");
-                    }
-                    else{
+                    } else {
                         $("#title").animate({'margin-top': '5px'}, 1000);
                         $(".book-list").css("visibility", "visible");
                         displayResults(response);
@@ -36,35 +32,20 @@ $(document).ready(function(){
     });
 
     $("#random-search").click(function() {
-        console.log("Random search clicked");
         outputList.innerHTML = "";
-    
         var randomGenres = ["Fiction", "Mystery", "Science", "History", "Romance", "Technology", "Biography", "Horror", "Fantasy", "Comedy", "Thriller", "Adventure", "Psychological"];
         var randomGenre = randomGenres[Math.floor(Math.random() * randomGenres.length)];
-    
-        var previouslyShownBooks = [];
-    
+
         $.ajax({
-            url: bookUrl + "subject:" + encodeURIComponent(randomGenre) + "&maxResults=1&langRestrict=en",
+            url: bookUrl + "subject:" + encodeURIComponent(randomGenre) + "&limit=1",
             dataType: "json",
             success: function(response) {
-                if (response.totalItems === 0) {
+                if (response.numFound === 0) {
                     alert("No results! Try again.");
                 } else {
-                    var uniqueResults = response.items.filter(function(book) {
-                        return !previouslyShownBooks.includes(book.id);
-                    });
-    
-                    if (uniqueResults.length > 0) {
-                        $("#title").animate({ 'margin-top': '5px' }, 1000);
-                        $(".book-list").css("visibility", "visible");
-                        previouslyShownBooks.push(...uniqueResults.map(function(book) {
-                            return book.id;
-                        }));
-                        displayResults({ items: uniqueResults, totalItems: uniqueResults.length });
-                    } else {
-                        alert("All results have been shown. Please try again.");
-                    }
+                    $("#title").animate({ 'margin-top': '5px' }, 1000);
+                    $(".book-list").css("visibility", "visible");
+                    displayResults(response);
                 }
             },
             error: function() {
@@ -72,25 +53,21 @@ $(document).ready(function(){
             }
         });
     });
-    
+
     $("#author-search").click(function(){
         outputList.innerHTML = "";
-        console.log("Author search clicked");
         searchData = $("#search-box").val();
 
         if(searchData === "" || searchData === null){
             displayError();
-        }
-        else{
+        } else {
             $.ajax({
-                url: bookUrl + "inauthor:" + '"' + encodeURIComponent(searchData) + '"' + "&printType=books&maxResults=6",
+                url: bookUrl + "author:" + encodeURIComponent(searchData) + "&limit=6",
                 dataType: "json",
                 success: function(response){
-                    console.log(response);
-                    if(response.totalItems === 0){
+                    if(response.numFound === 0){
                         alert("No results! Try again.");
-                    }
-                    else{
+                    } else {
                         $("#title").animate({'margin-top': '5px'}, 1000);
                         $(".book-list").css("visibility", "visible");
                         displayResults(response);
@@ -105,43 +82,35 @@ $(document).ready(function(){
     });
 
     function displayResults(response) {
-        const books = response.items;
+        const books = response.docs;
         let rowHTML = '';
         for (let i = 0; i < books.length; i++) {
-            let book = books[i].volumeInfo;
+            let book = books[i];
             let title = book.title || "No Title Available";
-            let author = book.authors ? book.authors.join(", ") : "Unknown Author";
-            let publisher = book.publisher || "Unknown Publisher";
-            let bookLink = book.infoLink || "#";
-            let bookImg = book.imageLinks && book.imageLinks.thumbnail
-                          ? book.imageLinks.thumbnail
+            let author = book.author_name ? book.author_name.join(", ") : "Unknown Author";
+            let publisher = book.publisher ? book.publisher[0] : "Unknown Publisher";
+            let bookLink = book.key ? `https://openlibrary.org${book.key}` : "#";
+            let bookImg = book.cover_i
+                          ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
                           : placeHldr;
-    
-            // Append the card to the rowHTML
+
             rowHTML += formatOutput(bookImg, title, author, publisher, bookLink);
-    
-            // Every 3 books or at the end of the list, close the row and append to outputList
+
             if ((i + 1) % 3 === 0 || i === books.length - 1) {
                 outputList.innerHTML += `<div class="row mt-4">${rowHTML}</div>`;
-                rowHTML = ''; // Reset for the next row
+                rowHTML = '';
             }
         }
     }
 
     function formatOutput(bookImg, title, author, publisher, bookLink) {
-        var placeholder = './assets/images/placeholder.jpeg';
-        var imageSrc = bookImg || placeholder;
-
-        var htmlCard = `<div class="col-lg-4 col-md-6 mb-4"> <!-- col-lg-4 for 3 items per row -->
+        var htmlCard = `<div class="col-lg-4 col-md-6 mb-4">
           <div class="card" id="random-card">
-            <div class="card-body d-flex flex-column justify-content-between"> <!-- Use flexbox for consistent layout -->
-              <!-- Book Image -->
-              <img src="${imageSrc}" class="card-img-top mx-auto" alt="Book Cover" style="max-width: 200px; margin-bottom: 20px;">
-              <!-- Title -->
+            <div class="card-body d-flex flex-column justify-content-between">
+              <img src="${bookImg}" class="card-img-top mx-auto" alt="Book Cover" style="max-width: 200px; margin-bottom: 20px;">
               <h5 class="card-title">${title}</h5>
-              <!-- Author -->
               <p class="card-text">Author: ${author}</p>
-              <!-- Book Link Button -->
+              <p class="card-text">Publisher: ${publisher}</p>
               <a target="_blank" href="${bookLink}" class="btn btn-secondary mt-3">View Book</a>
             </div>
           </div>
